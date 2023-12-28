de6753f5878260192ee4b7db4c8192412bdeeb2c
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2023-12-28 20:01:21 +0100

    Working code execution

--- mini-bots/Program.cs
+++ mini-bots/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
-using System.Threading.Tasks;
+using System.IO; // Need to add this
 using DSharpPlus;
+using DSharpPlus.Entities;
+// using DSharpPlus.SlashCommands;
+using NLua;
 
 namespace MyFirstBot
 {
@@ -8,21 +11,96 @@ namespace MyFirstBot
     {
         static async Task Main(string[] args)
         {
+            string token;
+            DiscordLua discordLua = new DiscordLua();
+
+            // Read the token from a file instead of having it in code
+            try
+            {
+                token = File.ReadAllText("./token.txt").Trim(); // Make sure to update the path to the actual file location
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Could not read the token file: {ex.Message}");
+                return;
+            }
+
             var discord = new DiscordClient(new DiscordConfiguration()
             {
-                Token = "MTE4OTk4MTYwNTQxMDg5Nzk1MA.Gvmv_q.QUcNequxF4lNCPlq07X54okIrLdNvoqaNl7F6Q",
+                Token = token,
                 TokenType = TokenType.Bot,
                 Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
             });
+            //var slash = discord.UseSlashCommands();
+            //slash.RegisterCommands<SlashCommands>(554977304665784325);
 
             discord.MessageCreated += async (s, e) =>
             {
-                if (e.Message.Content.ToLower().StartsWith("ping"))
-                    await e.Message.RespondAsync("pong!");
+                // If message is from bot, ignore
+                if (e.Author.IsBot) return;
+
+                string discordMessage = e.Message.Content;
+
+
+                Console.WriteLine(discordMessage);
+                if (discordMessage.StartsWith("!bot"))
+                {
+                    string code = discordMessage.Substring(5).Trim();
+                    code = code.Replace("```lua", "");
+                    code = code.Replace("```", "");
+                    Console.WriteLine("Running lua: " + code);
+                    try
+                    {
+                        await e.Message.RespondAsync(discordLua.Run(code));
+                    }
+                    catch (Exception ex)
+                    {
+                        await e.Message.RespondAsync(ex.Message);
+                    }
+                }
+                else
+                {
+                    await e.Message.RespondAsync("Pucko");
+                }
+
             };
 
             await discord.ConnectAsync();
             await Task.Delay(-1);
+        }
+    }
+
+    // Lua class
+    public class DiscordLua
+    {
+        private Lua lua;
+
+        public DiscordLua()
+        {
+            lua = new Lua();
+        }
+
+        public string Run(string input)
+        {
+            object[] luaOutput = lua.DoString(input);
+
+            if (luaOutput.Length > 0)
+            {
+                var stringOut = luaOutput[0].ToString();
+                if (stringOut != null)
+                {
+                    return stringOut;
+                }
+                else
+                {
+                    return "No output";
+                }
+            }
+            else
+            {
+                return "No output";
+            }
+
         }
     }
 }
