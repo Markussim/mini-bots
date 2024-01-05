a0440410344e59257390f3b812d91953e936c8f0
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2024-01-05 13:57:54 +0100

    Revert "prettier commit"
    This reverts commit 1717d81f193a73b42101a7702764cdf048c2eb41.

--- mini-bots/Program.cs
+++ mini-bots/Program.cs
@@ -5,24 +5,25 @@ using Watson.ORM.Sqlite;
 using Watson.ORM.Core;
 using ExpressionTree;
 using DatabaseWrapper.Core;
+using System.Net.Http;
 
 // Apply attributes to your class
 [Table("miniBots")]
 public class MiniBot
 {
-    [Column("id", true, DataTypes.Int, false)]
-    public int Id { get; set; }
+  [Column("id", true, DataTypes.Int, false)]
+  public int Id { get; set; }
 
-    [Column("name", false, DataTypes.Nvarchar, 64, false)]
-    public string Name { get; set; }
+  [Column("name", false, DataTypes.Nvarchar, 64, false)]
+  public string Name { get; set; }
 
-    [Column("code", false, DataTypes.Nvarchar, 64, false)]
-    public string Code { get; set; }
+  [Column("code", false, DataTypes.Nvarchar, 64, false)]
+  public string Code { get; set; }
 
-    // Parameter-less constructor is required
-    public MiniBot()
-    {
-    }
+  // Parameter-less constructor is required
+  public MiniBot()
+  {
+  }
 }
 
 namespace MiniBots
@@ -97,8 +98,7 @@ namespace MiniBots
                     bool miniBotExists = false;
                     MiniBot? oldMiniBot = GetMiniBotByName(orm, name);
 
-                    if (oldMiniBot != null)
-                    {
+                    if (oldMiniBot != null){
                         oldMiniBot.Code = code;
                         orm.Update<MiniBot>(oldMiniBot);
 
@@ -107,7 +107,7 @@ namespace MiniBots
 
                     if (!miniBotExists)
                     {
-                        MiniBot miniBot = new MiniBot { Name = name, Code = code };
+                        MiniBot miniBot = new MiniBot { Name=name, Code=code };
                         orm.Insert<MiniBot>(miniBot);
                     }
                 }
@@ -132,7 +132,7 @@ namespace MiniBots
                     // Select all records
                     List<MiniBot> miniBots = orm.SelectMany<MiniBot>();
 
-                    foreach (MiniBot miniBot in miniBots)
+                    foreach(MiniBot miniBot in miniBots)
                     {
                         message += "- " + miniBot.Name + "\n";
                     }
@@ -145,8 +145,7 @@ namespace MiniBots
 
                     MiniBot? miniBot = GetMiniBotByName(orm, name);
 
-                    if (miniBot != null)
-                    {
+                    if (miniBot != null){
                         SendDiscordMessage("```" + miniBot.Code + "```", e);
                     }
                 }
@@ -155,13 +154,11 @@ namespace MiniBots
                     string name = discordMessage.Substring(7).Trim();
                     MiniBot? miniBot = GetMiniBotByName(orm, name);
 
-                    if (miniBot != null)
-                    {
+                    if (miniBot != null){
                         orm.Delete<MiniBot>(miniBot);
                         SendDiscordMessage("Deleted: " + name, e);
                     }
-                    else
-                    {
+                    else{
                         SendDiscordMessage("No bot with name: " + name, e);
                     }
                 }
@@ -172,8 +169,7 @@ namespace MiniBots
                     // Select all records
                     List<MiniBot> miniBots = orm.SelectMany<MiniBot>();
 
-                    foreach (MiniBot miniBot in miniBots)
-                    {
+                    foreach(MiniBot miniBot in miniBots){
                         string botOutput = "";
                         try
                         {
@@ -198,8 +194,7 @@ namespace MiniBots
             await Task.Delay(-1);
         }
 
-        public static MiniBot? GetMiniBotByName(WatsonORM orm, string name)
-        {
+        public static MiniBot? GetMiniBotByName(WatsonORM orm, string name){
             Expr selectFilter = new Expr(
                 orm.GetColumnName<MiniBot>(nameof(MiniBot.Name)),
                 OperatorEnum.Equals,
@@ -207,13 +202,11 @@ namespace MiniBots
             // Select all records
             List<MiniBot> miniBots = orm.SelectMany<MiniBot>(null, null, selectFilter);
 
-            if (miniBots.Count > 1)
-            {
+            if (miniBots.Count > 1){
                 // This should never happen
                 Console.WriteLine("More than 1 bot with name: " + name);
             }
-            if (miniBots.Count <= 0)
-            {
+            if (miniBots.Count <= 0){
                 return null;
             }
 
@@ -233,6 +226,31 @@ namespace MiniBots
         }
     }
 
+    public class HttpManager
+    {
+        public async Task<string> MakeRequest(string url){
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                using HttpResponseMessage response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+
+                Console.WriteLine(responseBody);
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return "http request failed";
+            }
+        }
+    }
+
     // Lua class
     public class DiscordLua
     {
@@ -246,6 +264,10 @@ namespace MiniBots
         public string Run(string code, string message = "")
         {
             Byte[] luaIn = Encoding.UTF8.GetBytes($"message = \"{message}\"\n" + code);
+
+            HttpManager httpManager = new HttpManager();
+            lua["httpManager"] = httpManager;
+
             // TODO: Escape message
             // TODO: Handle utf8 output
             object[] luaOutput = lua.DoString(luaIn);
